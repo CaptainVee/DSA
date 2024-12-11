c9ad43bcd54072618dd8b5e3f40bbbc69c205386
CaptainVee <captainvee3@gmail.com>
2024-12-11 16:59:36 +0100

    solution for tornament winner

---
+++ tornament_winner/solution.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+string tournamentWinner(
+  vector<vector<string>> competitions, vector<int> results
+) {
+  string best_team {""};
+  map<string, int> hash {};
+  
+  for(int i {0}; i < results.size(); i++){
+    if(results[i] == 0){
+      string winner = competitions[i][1];
+        auto search = hash.find(winner);
+        if (search == hash.end()) {
+            hash[winner] = 3;
+            if (hash[winner] > hash[best_team]){
+              best_team = winner;
+            }
+        
+            } 
+        else {
+                hash[winner] = hash[winner] + 3 ;
+              if (hash[winner] > hash[best_team]){
+              best_team = winner;
+            }
+              }
+    }else{
+        string winner = competitions[i][0];
+        auto search = hash.find(winner);
+        if (search == hash.end()) {
+            hash[winner] = 3;
+              if (hash[winner] > hash[best_team]){
+              best_team = winner;
+            }
+              } 
+        else {
+              hash[winner] = hash[winner] + 3;
+              if (hash[winner] > hash[best_team]){
+              best_team = winner;
+            }
+              }
+    }
+  }
+  return best_team;
+}
