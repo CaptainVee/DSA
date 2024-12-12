4ba152f896d2c2fcf98418d4b34dbd8164919360
CaptainVee <captainvee3@gmail.com>
2024-12-12 12:30:53 +0100

    updated algorithm

--- tornament_winner/solution.cpp
+++ tornament_winner/solution.cpp
@@ -7,40 +7,75 @@ string tournamentWinner(
 ) {
   string best_team {""};
   map<string, int> hash {};
+  string winner {};
   
   for(int i {0}; i < results.size(); i++){
     if(results[i] == 0){
-      string winner = competitions[i][1];
-        auto search = hash.find(winner);
-        if (search == hash.end()) {
-            hash[winner] = 3;
-            if (hash[winner] > hash[best_team]){
-              best_team = winner;
-            }
-        
-            } 
-        else {
-                hash[winner] = hash[winner] + 3 ;
-              if (hash[winner] > hash[best_team]){
-              best_team = winner;
-            }
-              }
+      winner = competitions[i][1];  
+
     }else{
-        string winner = competitions[i][0];
-        auto search = hash.find(winner);
-        if (search == hash.end()) {
-            hash[winner] = 3;
-              if (hash[winner] > hash[best_team]){
-              best_team = winner;
-            }
-              } 
-        else {
-              hash[winner] = hash[winner] + 3;
-              if (hash[winner] > hash[best_team]){
-              best_team = winner;
-            }
-              }
+      winner = competitions[i][0];
     }
+
+    hash[winner]+=3;
+    if (hash[winner] > hash[best_team]){
+        best_team = winner;
+      }
   }
   return best_team;
 }
+
+
+
+
+
+
+
+// more code solution
+
+// #include <vector>
+// #include <map>
+// using namespace std;
+
+// string tournamentWinner(
+//   vector<vector<string>> competitions, vector<int> results
+// ) {
+//   string best_team {""};
+//   map<string, int> hash {};
+  
+//   for(int i {0}; i < results.size(); i++){
+//     if(results[i] == 0){
+//       string winner = competitions[i][1];
+//         auto search = hash.find(winner);
+//         if (search == hash.end()) {
+//             hash[winner] = 3;
+//             if (hash[winner] > hash[best_team]){
+//               best_team = winner;
+//             }
+        
+//             } 
+//         else {
+//                 hash[winner] = hash[winner] + 3 ;
+//               if (hash[winner] > hash[best_team]){
+//               best_team = winner;
+//             }
+//               }
+//     }else{
+//         string winner = competitions[i][0];
+//         auto search = hash.find(winner);
+//         if (search == hash.end()) {
+//             hash[winner] = 3;
+//               if (hash[winner] > hash[best_team]){
+//               best_team = winner;
+//             }
+//               } 
+//         else {
+//               hash[winner] = hash[winner] + 3;
+//               if (hash[winner] > hash[best_team]){
+//               best_team = winner;
+//             }
+//               }
+//     }
+//   }
+//   return best_team;
+// }
